c0a46d0237ee31ab7aa465342f980129b2831458
daniyalrosli <94339715+daniyalrosli@users.noreply.github.com>
2024-08-04 22:14:26 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage",page_icon=":tada",layout="wide")
+
+
+st.subheader("Hi, I'm Daniyal Rosli")
+st.title("A Intelligent System Engineering Student")
+st.write("I am passionate about artificial intelligence, machine learning, data analysis and data visualization")
+st.write("[Learn More>](https://daniyal.com)")
+
